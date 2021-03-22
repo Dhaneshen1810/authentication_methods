98f6e933840f1ca58bbcab5eac7b93d66252b63d
Dhaneshwaran Moonian <dan.moonian@gmail.com>
2021-03-21 19:38:37 -0600

    add google login

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 const user = require('../models/user');
 var router = express.Router();
 
+var passwordHash = require('password-hash');
+
 // import model
 const User = require('../models/user');
+
+// function hashes password
+function hashPassword(password){
+  var hashedPassword = passwordHash.generate(password);
+  return hashedPassword
+}
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
@@ -15,20 +23,30 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+
+// add new user
 router.post('/', async (req, res) => {
   try {
-    let new_user = new User(req.body);
-    console.log(new_user)
-    const savedPost = await new_user.save()
-    .then(data => {
-      res.json(data)
+    // password must be hased before inserting user credentials in database
+    req.body.password = hashPassword(req.body.password);  // hash password
+
+    // Create new entry
+    let newEntry = new User({
+      username: req.body.username,
+      password: req.body.password
+    })
+
+    // insert new entry into database
+    const savedPost = await newEntry.save()
+    .then(response => {
+      res.status(200).json({ success:true })
     })
     .catch(err => {
-      console.log(err)
-      res.json({ message: err })
+      res.json({ success: false })
     })
     } catch (error) {
-    res.json({message: error})
+      console.log(error)
+      res.status(400).json({ success:false })   
   }
 })
 
@@ -38,11 +56,17 @@ router.get('/:username', async function(req, res, next) {
     // initialize variables
     let username = req.params.username; // username
     let password = req.query.password;  // password
-    // retrieve from databasen using username password as filter
-    const users = await User.find({username: username, password: password})
 
+    // verify if password matches hashed database version
+    // user entry is retrieved using username
+    // the password is then verified using the "password-hash" library "verify" function
+    const users = await User.find({ username: username })
+    let passwordCheck = passwordHash.verify(password, users[0].password);
 
-    if (users.length === 1 ){ // user is found/verified
+    // the user is verified only if there are no duplicate user ans
+    // the password passes the hash verification.
+    // Otherwise the 400 error is returned
+    if (users.length === 1 && passwordCheck === true){ // user is found and verified
       res.status(200).json({ response: users });    
     }
     else{ // one or more credentials is not valid
